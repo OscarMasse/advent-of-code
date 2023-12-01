242f3c95dbdd3c95b789dbd99985119b4f7fa126
Oscar Masse <oscar@masse.pro>
2023-12-01 17:49:02 +0100

    add year argument to generator

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# advent-of-code-2022
-https://adventofcode.com/2022
+# advent-of-code
+https://adventofcode.com/
 
 My solutions are in the branch *[oscar](https://github.com/OscarMasse/advent-of-code-2022/tree/oscar)*.
 
 # Installation
 Paste your session cookie in `session_cookie.txt` (without `session=`)
 
+Use the year of your choice:
 ```
-python ./generator.py
+python ./generator.py 2022
 ```
--- generator.py
+++ generator.py
@@ -1,40 +1,41 @@
 import os
 import shutil
+import sys
 
 import requests as requests
 
 from utils import load_session_cookie
 
 
-def generate(session_cookie=load_session_cookie()):
+def generate(year: int, session_cookie=load_session_cookie()):
     for number in range(1, 26):
         print(f"Generating Day {number:02}")
         dir_name = f"day{number:02}"
 
         response = None
-        if not os.path.isfile(f"{dir_name}/input.txt"):
-            response = requests.get(f"https://adventofcode.com/2022/day/{number}/input",
+        if not os.path.isfile(f"{year}/{dir_name}/input.txt"):
+            response = requests.get(f"https://adventofcode.com/{year}/day/{number}/input",
                                     headers={"cookie": f"session={session_cookie}"})
             if not response.status_code == 200:
                 continue
 
         # Directory
-        if not os.path.isdir(f"{dir_name}"):
+        if not os.path.isdir(f"{year}/{dir_name}"):
             print(f"• Creating directory")
-            os.makedirs(f"{dir_name}", exist_ok=True)
+            os.makedirs(f"{year}/{dir_name}", exist_ok=True)
 
         # Input file
         if response:
             print(f"• Creating input.txt")
-            with open(f"{dir_name}/input.txt", "wb") as file:
+            with open(f"{year}/{dir_name}/input.txt", "wb") as file:
                 file.write(response.content)
 
         # Python files
         for file_name in ["part_one.py", "part_two.py"]:
             if not os.path.isfile(f"{dir_name}/{file_name}"):
                 print(f"• Copying {file_name}")
-                shutil.copy(f"templates/{file_name}", f"{dir_name}/{file_name}")
+                shutil.copy(f"templates/{file_name}", f"{year}/{dir_name}/{file_name}")
 
 
 if __name__ == "__main__":
-    generate()
+    generate(int(sys.argv[1]))
