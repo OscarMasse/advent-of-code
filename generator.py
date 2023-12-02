9680e9a7d0cde3abdf00f4856420a9438f038ad7
Oscar Masse <oscar@masse.pro>
2023-12-02 11:42:07 +0100

    fix generator

--- generator.py
+++ generator.py
@@ -13,28 +13,32 @@ def generate(year: int, session_cookie=load_session_cookie()):
         dir_name = f"day{number:02}"
 
         response = None
-        if not os.path.isfile(f"{year}/{dir_name}/input.txt"):
-            response = requests.get(f"https://adventofcode.com/{year}/day/{number}/input",
-                                    headers={"cookie": f"session={session_cookie}"})
+        if not os.path.isfile(f"year{year}/{dir_name}/input.txt"):
+            response = requests.get(
+                f"https://adventofcode.com/{year}/day/{number}/input",
+                headers={"cookie": f"session={session_cookie}"},
+            )
             if not response.status_code == 200:
                 continue
 
         # Directory
-        if not os.path.isdir(f"{year}/{dir_name}"):
+        if not os.path.isdir(f"year{year}/{dir_name}"):
             print(f"• Creating directory")
-            os.makedirs(f"{year}/{dir_name}", exist_ok=True)
+            os.makedirs(f"year{year}/{dir_name}", exist_ok=True)
 
         # Input file
         if response:
             print(f"• Creating input.txt")
-            with open(f"{year}/{dir_name}/input.txt", "wb") as file:
+            with open(f"year{year}/{dir_name}/input.txt", "wb") as file:
                 file.write(response.content)
 
         # Python files
         for file_name in ["part_one.py", "part_two.py"]:
-            if not os.path.isfile(f"{dir_name}/{file_name}"):
+            if not os.path.isfile(f"year{year}/{dir_name}/{file_name}"):
                 print(f"• Copying {file_name}")
-                shutil.copy(f"templates/{file_name}", f"{year}/{dir_name}/{file_name}")
+                shutil.copy(
+                    f"templates/{file_name}", f"year{year}/{dir_name}/{file_name}"
+                )
 
 
 if __name__ == "__main__":
